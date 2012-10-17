3a65d81b90a42b6d4ddaf11c81cc9f6bfa3309ca
Hassan McKusick <Hassan.McKusick@gmail.com>
2012-10-17 15:32:35 -0400

    Adds MongoMapper gem and mongo.yml

---
+++ app/models/session_data.rb
@@ -0,0 +1,5 @@
+class SessionData
+  include MongoMapper::Document
+
+
+end
---
+++ test/unit/session_data_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SessionDataTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
